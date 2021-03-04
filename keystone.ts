25cd571dabe8ea59b6bc9d2cf5f5635f3bdeee01
Peter Ayeni <ayenicology@gmail.com>
2021-03-04 19:41:39 +0000

    feat: Cart Schema

--- keystone.ts
+++ keystone.ts
@@ -10,6 +10,7 @@ import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
 import { insertSeedData } from "./seed-data";
 import { sendPasswordResetEmail } from "./lib/mail";
+import { CartItem } from "./schemas/CartItem";
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -57,6 +58,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     ui: {
       // show the UI for people who pass the test
